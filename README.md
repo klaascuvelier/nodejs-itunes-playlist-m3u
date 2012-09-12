e26e6c818a61a73d05157cc26f4f8d35d2a6ee4d
Klaas Cuvelier <cuvelierklaas@gmail.com>
2012-09-12 17:01:23 +0200

    update README and code to be more generic

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
-nodejs-itunes-playlist-to-xbmc
+nodejs-itunes-playlist-to-m3u
 ==============================
 
-Convert iTunes playlists to m3u files for XBMC.
+Convert iTunes playlists to m3u files.
+
 Works with Smart playlists (top rated and most played) and Genius playlists too.
+
+usage
+=====
+Default:
+```node index.js```
+
+Possible parameters:
+
+`--music, -m` specify the directory where the iTunes folder is in. Default value: `~/Music`
+
+`--playlist, -p` the directory where the m3u files should be placed. Default value: `~/Music`
+
+`--directory, -d` an option to replace the original directory with another directory. This might come in handy if you're exporting the playlists to use on another device. Default value: the value of `--music`
+
+Example:
+```node index.js -m ~/Music -p ~/ -d /home/xbmc/Music```
--- index.js
+++ index.js
@@ -1,74 +1,140 @@
-var plist           = require('plist'),
-    fs              = require('fs'),
+(function start(process, require, console) {
+    "use strict";
 
-    library         = void 0,
+    var plist           = require('plist'),
+        fs              = require('fs'),
 
-    musicFolder     = '/Users/klaascuvelier/Music',
-    remoteFolder    = '/home/xbmc/Music',
-    playlistFolder  = '/Users/klaascuvelier'
-    ;
+        library         = void 0,
+
+        musicFolder     = void 0,
+        remoteFolder    = void 0,
+        playlistFolder  = void 0;
 
 
-readItunesLibraryXML(getPlayLists);
+    /**
+     * Get parameters via arguments
+     */
+    function getParameters(process) {
+        var homeFolder  = process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME'],
+            args        = process.argv.splice(2),
+            l           = void 0,
+            i           = void 0;
 
+        args    = args.join(' ').split(/[\s=]+/);
+        l       = args.length;
 
-/**
- * Read iTunes Library XML file (PLIST);
- *
- * @param method callback
- * @return void
- */
-function readItunesLibraryXML(callback)
-{
-    plist.parseFile(musicFolder + "/iTunes/iTunes Music Library.xml", function (error, data) {
+        musicFolder     = homeFolder + '/Music';
+        remoteFolder    = homeFolder + '/Music';
+        playlistFolder  = homeFolder + '/Music';
 
-        if (!error)
-        {
-            /*
-            Major Version
-            Minor Version
-            Date
-            Application Version
-            Features
-            Show Content Ratings
-            Music Folder
-            Library Persistent ID
-            Tracks
-            Playlists
-             */
+        for (i = 0; i < l; i += 2) {
 
-            library = data[0];
+            switch (args[i]) {
+            case '--music':
+            case '-m':
+                musicFolder     = args[(i + 1)].replace('~', homeFolder);
+                remoteFolder    = musicFolder;
+                break;
 
-            if (callback) {
-                callback();
+            case '--playlists':
+            case '-p':
+                playlistFolder  = args[(i + 1)].replace('~', homeFolder);
+                break;
+
+            case '--directory':
+            case '-d':
+                remoteFolder    = args[(i + 1)].replace('~', homeFolder);
+                break;
             }
+
         }
-        else {
-            console.log('Could not read the iTunes Music Library', error);
+    }
+
+
+    /**
+     * Read iTunes Library XML file (PLIST);
+     *
+     * @param method callback
+     * @return void
+     */
+    function readItunesLibraryXML(callback) {
+
+        try {
+
+
+            plist.parseFile(musicFolder + "/iTunes/iTunes Music Library.xml", function (error, data) {
+
+                if (!error) {
+                    /*
+                    Major Version
+                    Minor Version
+                    Date
+                    Application Version
+                    Features
+                    Show Content Ratings
+                    Music Folder
+                    Library Persistent ID
+                    Tracks
+                    Playlists
+                     */
+
+                    library = data[0];
+
+                    if (callback) {
+                        callback();
+                    }
+                } else {
+                    console.log('Could not parse the iTunes Music Library', error);
+                }
+
+            });
+        } catch (exception) {
+            console.log('Could not read the iTunes Music Library', exception);
         }
 
-    });
+    }
 
-}
 
-/**
- * Get playlists from Library
- * @return {[type]}
- */
-function getPlayLists() {
+    /**
+     * Create a playlist (M3U-format)
+     * @param  {String} name
+     * @param  {Array} tracks
+     * @return void
+     */
+    function createPlayList(name, tracks) {
 
-    var playlists   = library.Playlists,
-        index       = void 0,
-        playlist    = void 0;
+        var output      = '',
+            i           = void 0,
+            length      = tracks.length,
+            addTrack    = function (trackId) {
+                var track   = library.Tracks[trackId];
 
-i = 0;
+                output += '#EXTINF:' + track['Total Time'] + ', ' + track.Artist + ' - ' + track.Name + "\n";
+                output += track.Location.replace('file://localhost' + musicFolder, remoteFolder).replace(/%20/g, ' ') + "\n\n";
+            };
 
-    for (index in playlists)
-    {
+        output += '#EXTM3U' + "\n";
 
-        if (playlists.hasOwnProperty(index)) {
+        for (i = 0; i < length; i += 1) {
+            addTrack(tracks[i]['Track ID']);
+        }
 
-            playlist    = playlists[index];
+        try {
+            fs.writeFileSync(playlistFolder + '/' + name.replace(/ /g, '-') + '.m3u', output, 'utf-8');
+        } catch (exception) {
+            console.log('Could not write m3u-file', exception);
+        }
+    }
+
+    /**
+     * Get playlists from Library
+     * @return {[type]}
+     */
+    function getPlayLists() {
+
+        var playlists   = library.Playlists,
+            index       = void 0,
+            playlist    = void 0,
             skip        = [
                 4,      // Music
                 2,      // Movies
@@ -81,42 +147,28 @@ i = 0;
                 200     // 90's Music
             ];
 
-            if (skip.indexOf(playlist['Distinguished Kind']) === -1 && playlist.Master !== true && typeof playlist['Playlist Items'] !== 'undefined')
-            {
-                createPlayList(playlist.Name, playlist['Playlist Items']);
+        for (index in playlists) {
+
+            if (playlists.hasOwnProperty(index)) {
+
+                playlist    = playlists[index];
+
+                if (skip.indexOf(playlist['Distinguished Kind']) === -1 && playlist.Master !== true && playlist['Playlist Items'] !== undefined) {
+                    createPlayList(playlist.Name, playlist['Playlist Items']);
+                }
+
             }
-           
+
         }
 
     }
 
-}
+
+    // initialize parameters
+    getParameters(process);
+
+    // start by reading the library
+    readItunesLibraryXML(getPlayLists);
 
 
-/**
- * Create a playlist (M3U-format)
- * @param  {String} name
- * @param  {Array} tracks
- * @return void
- */
-function createPlayList(name, tracks) {
-
-    var output      = '',
-        i           = void 0,
-        length      = tracks.length,
-        addTrack    = function (trackId) {
-            var track   = library.Tracks[trackId];
-
-            output += '#EXTINF:' + track['Total Time'] + ', ' + track.Artist + ' - ' + track.Name + "\n";
-            output += track.Location.replace('file://localhost' + musicFolder, remoteFolder).replace(/%20/g, ' ') + "\n\n";
-        };
-
-    output += '#EXTM3U' + "\n";
-
-    for (i = 0; i < length; i++)
-    {
-        addTrack(tracks[i]['Track ID']);
-    }
-    
-    fs.writeFileSync(playlistFolder + '/' + name.replace(/ /g, '-') + '.m3u', output, 'utf-8');
-}
+})(process, require, console);
